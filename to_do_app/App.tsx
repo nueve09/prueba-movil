ae8ec5b7e612815a6b57fffe5410c070404eade1
Cesar Alberto Roman Reyes <cesaralbertoromanreyes@Mac-mini-de-Cesar.local>
2025-01-07 01:27:32 -0600

    [Login] Login assets are included in project

--- to_do_app/App.tsx
+++ to_do_app/App.tsx
@@ -1,11 +1,14 @@
-import { Provider } from 'react-redux'
-import { store } from './src/Presentation/store/store'
-import Main from './src/main'
+import {Provider} from 'react-redux';
+import {store} from './src/Presentation/store/store';
+import Main from './src/Presentation/main';
+import {SafeAreaView, Text, View} from 'react-native';
 
 export default function App() {
   return (
-    <Provider store={ store }>
-      <Main/>
+    <Provider store={store}>
+      <SafeAreaView style={{flex: 1}}>
+        <Main />
+      </SafeAreaView>
     </Provider>
-  )
+  );
 }
