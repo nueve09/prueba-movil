c9e6f848c2ee9e38e4bee6289afb367923875812
Cesar Alberto Roman Reyes <cesaralbertoromanreyes@Mac-mini-de-Cesar.local>
2025-01-08 21:07:48 -0600

    [VIEW MODEL] globalMoldal view model was moves to view model folder

--- to_do_app/src/Presentation/viewmodels/useGloblaModalViewModel.ts
+++ to_do_app/src/Presentation/viewmodels/useGloblaModalViewModel.ts
@@ -1,23 +1,29 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { setLogOut } from '../../store/slices/user/userSlice';
-import { removeTask } from '../../store/slices/task/taskSlice';
-import { RootState } from '../../store/store';
-import { hideModal } from '../../store/slices/modal/modalSlice';
+import { setLogOut } from '../store/slices/user/userSlice';
+import { removeTask } from '../store/slices/task/taskSlice';
+import { RootState } from '../store/store';
+import { hideModal, setActionDone } from '../store/slices/modal/modalSlice';
 
 export const useGloblaModalViewModel = () => {
 
     const dispatch = useDispatch();
     const { taskList ,  filteredList} = useSelector((state:RootState) => state.task);
-    const { params } = useSelector((state:RootState) => state.modal);
+    const { params , visible , actionDone} = useSelector((state:RootState) => state.modal);
 
     const removeItem = () => {
         const newFilteredList = filteredList.filter(task=> task.id !== params!.taskId)
         const newList = taskList.filter(task=> task.id !== params!.taskId)
     
         dispatch(removeTask({filteredList:newFilteredList,taskList:newList}))
+        dispatch(setActionDone({actionDone:true}))
         dispatch(hideModal());
+        
       };
+    
+    const editItem = () => {
+      
+    }
 
     const actionsHandler:Record<string,any> = {
         'logout':()=>{
@@ -25,10 +31,11 @@ export const useGloblaModalViewModel = () => {
             dispatch(hideModal())
         },
         'removeItem':()=>removeItem(),
-        'hide':()=>dispatch(hideModal())
+        'editItem':()=>editItem(),
+        'hide':()=>dispatch(hideModal()),
     }
 
 
 
-  return {actionsHandler}
+  return {actionsHandler, visible, params ,actionDone}
 }
