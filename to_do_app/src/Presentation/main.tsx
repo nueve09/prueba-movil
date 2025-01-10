fbe39e58c915113594a535997c76063576794540
Cesar Alberto Roman Reyes <cesaralbertoromanreyes@Mac-mini-de-Cesar.local>
2025-01-10 06:42:02 -0600

    [GRADIENT] se modifica la validacion de userId en Main.tsx

--- to_do_app/src/Presentation/main.tsx
+++ to_do_app/src/Presentation/main.tsx
@@ -3,15 +3,13 @@ import {HomeNavigation} from './navigation/StackNavigation';
 import {useSelector} from 'react-redux';
 import {RootState} from './store/store';
 import Login from './screens/Login';
-import GlobalModal from './components/modals/globalModal';
 
 export default function Main() {
   const { userId } = useSelector((state: RootState) => state.user);
 
   return (
     <View style={{flex: 1}}>
-      <GlobalModal />
-      {userId !== '' ? <HomeNavigation /> : <Login />}
+      {userId !== null ? <HomeNavigation /> : <Login />}
     </View>
   );
 }
