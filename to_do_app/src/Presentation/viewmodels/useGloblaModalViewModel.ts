54cfdbf796f13256990d5e95313289161c94a349
Cesar Alberto Roman Reyes <cesaralbertoromanreyes@192.168.1.114>
2025-01-12 11:40:16 -0600

    [Logout] Logout in taskscreen is fixed

--- to_do_app/src/Presentation/viewmodels/useGloblaModalViewModel.ts
+++ to_do_app/src/Presentation/viewmodels/useGloblaModalViewModel.ts
@@ -3,6 +3,7 @@ import { removeTask } from '../store/slices/task/taskSlice';
 import { AppDispatch, RootState } from '../store/store';
 import { hideModal } from '../store/slices/modal/modalSlice';
 import { TaskApi } from '../../Data/sources/api/remote/TaskApiSlice';
+import { setLogOut } from '../store/slices/user/userSlice';
 
 export const useGloblaModalViewModel = () => {
     const dispatch = useDispatch<AppDispatch>();
@@ -10,6 +11,7 @@ export const useGloblaModalViewModel = () => {
     const { params , visible } = useSelector((state:RootState) => state.modal);
 
     const removeItem = (onHide:()=>void) => {
+      if(!params?.taskId)return dispatch(setLogOut())
         const newFilteredList = filteredList.filter(task=> task.id !== params!.taskId)
         dispatch(removeInCacheData())
         dispatch(removeTask({filteredList:newFilteredList}))
